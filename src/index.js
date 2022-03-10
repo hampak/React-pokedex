a629b683da026a76e8659f07307c79cb4b609ade
Haemin Park <85291626+hampak@users.noreply.github.com>
2022-03-10 19:20:07 +0900

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 
 ReactDOM.render(<App/>, document.getElementById("root"))
-
-//https://stackoverflow.com/questions/62272410/store-values-of-multiple-api-calls-on-same-state-variable-on-react-native 참고
